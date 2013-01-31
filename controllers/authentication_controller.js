24e833602c2621176efea46821e20a147e70af1a
Narciso Guillen <narciso.guillen@tangosource.com>
2013-01-31 17:15:19 -0600

    redirect to home when login  / Create logout method

--- controllers/authentication_controller.js
+++ controllers/authentication_controller.js
@@ -20,16 +20,23 @@ module.exports = {
 
           req.logIn(user, function(err) {
             if (err) { return next(err); }
-            return res.send('Success', req.user);
+            var userEmail = req.user ? req.user.email : null;
+            return res.redirect('/');
+            // return res.send('Success', userEmail);
           });
 
         })(req, res, next);
       break;
 
-      default: res.render('/');
+      default: return res.redirect('/');
 
     }
 
+  },
+
+  logout: function(req, res){
+    req.logout();
+    res.redirect('/');
   }
 
 
