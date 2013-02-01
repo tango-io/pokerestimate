79d8576b13f07ff6ace26fb71ac0e043e706c203
Narciso Guillen <narciso.guillen@tangosource.com>
2013-01-31 19:17:31 -0600

    Logic to create a new user

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 var database = require("../config/database");
+var pivotal  = require("../libs/pivotal");
+var bcrypt   = require('bcrypt');
 var inspect  = require('eyes').inspector({ stream: null });
 
 module.exports = {
@@ -26,7 +28,28 @@ module.exports = {
   },
 
   create: function(data, callback){
-    return callback(null, data);
+
+    bcrypt.hash(data.password, 10, function(err, hash) {
+      data.password = hash;
+    });
+
+    pivotal.access({username: data.email, password: data.password}, function(err, result){
+      if(result.message){ return callback(err, result); }
+
+      data.id    = result.token.id[0]._;
+      data.token = result.token.guid[0];
+
+      database.open(function(err, db){
+        db.collection('users', function(error , userCollection){
+          userCollection.save(data, {safe: true}, function(err, user){
+            database.close();
+            return callback(err, user);
+          });
+        });
+      });
+
+    });
+
   }
 
 };
