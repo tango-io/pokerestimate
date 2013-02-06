c88a5640c438e8f101ec0b31fea3fb4b5e11d4b2
Narciso Guillen <narciso.guillen@tangosource.com>
2013-02-05 18:53:33 -0600

    Autofetxh projects

--- public/javascripts/app/views/homeView.js
+++ public/javascripts/app/views/homeView.js
@@ -3,7 +3,7 @@ TXE.Views.HomeView = Backbone.View.extend({
   collection: new TXE.Collections.Projects(),
 
   initialize: function(){
-    this.collection.bind('reset', this.render, this);
+    this.collection.bind('reset', this.render, this).fetch();
   },
 
   render: function(){
