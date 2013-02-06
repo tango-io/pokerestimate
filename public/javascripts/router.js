a602f04cabc092bf04278570b7b09ef7f9352015
Narciso Guillen <narciso.guillen@tangosource.com>
2013-02-05 18:25:34 -0600

    Backbone router

---
+++ public/javascripts/router.js
@@ -0,0 +1,24 @@
+TXE.Router = Backbone.Router.extend({ 
+
+  routes: {
+    '' : 'home'
+  },
+
+  initialize: function(){
+    var user = $('#user').val();
+
+    this.user = {
+      logged: user === 'null' ? false : true,
+      email:  user
+    };
+
+  },
+
+  home: function(){
+    if(this.user.logged){
+      this.homeView = new TXE.Views.HomeView();
+    }
+  }
+
+});
+
