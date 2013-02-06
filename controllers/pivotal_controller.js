c6abc64ef69c1dd2a28ff8c46e0d397cf8606ee7
Narciso Guillen <narciso.guillen@tangosource.com>
2013-02-05 19:25:45 -0600

    Build pivotal lib method to get projects

--- controllers/pivotal_controller.js
+++ controllers/pivotal_controller.js
@@ -1,5 +1,14 @@
+var pivotal  = require("../libs/pivotal");
+var inspect  = require('eyes').inspector({ stream: null });
+
 module.exports = {
   projects: function(req, res, next){
+    var token = req.user ? req.user.token : null;
+
+    pivotal.getProjects(token, function(error, data){
+      console.log(inspect(data));
+    });
+
     res.send([{name: 'one'}, {name: 'two'}, {name: 'three'}]);
   }
 };
--- libs/pivotal.js
+++ libs/pivotal.js
@@ -1,3 +1,4 @@
+var http        = require('http');
 var https       = require('https');
 var inspect     = require('eyes').inspector({ stream: null });
 var querystring = require('querystring');
@@ -39,6 +40,38 @@ module.exports.access = function(data, callback){
 
   req.on('error', function(e) {
     return callback.call(this, e);
+  });
+
+};
+
+module.exports.getProjects = function(token, callback){
+  var parser = new xml2js.Parser();
+
+  var options = {
+    host: 'www.pivotaltracker.com',
+    port: 80,
+    path: '/services/v3/projects',
+    method: 'GET',
+    headers: {  
+      'X-TrackerToken': token
+    }  
+  };
+
+  parser.addListener('end', function(result) {
+    return callback(null, result);
+  });
+
+  var req = http.request(options, function(res) {
+    res.on('data', function(d) {
+      parser.parseString(d);
+    });
+
+  });
+
+  req.end();
+
+  req.on('error', function(e) {
+    return callback(err);
   });
 
 };
