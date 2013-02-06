6f29eee566089cd35fa05924520bf220412c0271
Narciso Guillen <narciso.guillen@tangosource.com>
2013-02-05 19:59:29 -0600

    Build projects api

--- controllers/pivotal_controller.js
+++ controllers/pivotal_controller.js
@@ -1,14 +1,28 @@
 var pivotal  = require("../libs/pivotal");
+var _        = require('lodash');
 var inspect  = require('eyes').inspector({ stream: null });
 
 module.exports = {
   projects: function(req, res, next){
     var token = req.user ? req.user.token : null;
+    if(!token){res.send({error: 'Not logged in'});}
 
     pivotal.getProjects(token, function(error, data){
-      console.log(inspect(data));
+      if(error){res.send(error);}
+
+      var projects = data.projects ? data.projects.project : [];
+
+      var result = _.map(projects, function(project){
+        return {
+          name:   typeof project.name   === 'object' ? project.name.pop()   : project.name,
+          id:     typeof project.id     === 'object' ? project.id.pop()     : project.id,
+          public: typeof project.public === 'object' ? project.public.pop() : project.public,
+        };
+      });
+
+      res.send(result);
+
     });
 
-    res.send([{name: 'one'}, {name: 'two'}, {name: 'three'}]);
   }
 };
