034d96ce2f69bf56962ef95b5a5197d47e97d67e
sace <jaime.gonzalez@tangosource.com>
2013-02-08 18:03:18 -0600

    removed console

--- public/javascripts/app/views/homeView.js
+++ public/javascripts/app/views/homeView.js
@@ -9,7 +9,6 @@ TXE.Views.HomeView = Backbone.View.extend({
   },
 
   render: function(){
-    console.log(this.collection);
     var template = this.template;
     _.each(this.collection.models,function(model){
       var project = template(model.toJSON());
