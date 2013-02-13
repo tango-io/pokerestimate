fbd8520dbc798f7c88c5f98d53031d38b212b734
Narciso Guillen <narciso.guillen@tangosource.com>
2013-02-13 12:19:50 -0600

    remove single project route

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,6 @@ module.exports = function(app, passport){
   app.get('/api/v1/me', userController.me);
 
   app.get('/api/v1/projects',     pivotalController.projects);
-  app.get('/api/v1/projects/:id', pivotalController.project);
 
   app.get('/api/v1/projects/:project/tasks',     pivotalController.tasks);
   app.get('/api/v1/projects/:project/tasks/:id', pivotalController.task);
