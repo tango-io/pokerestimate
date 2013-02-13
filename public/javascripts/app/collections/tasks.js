b3683d9c6a8266c58448c4f6ad9b64545f2f1b9e
Narciso Guillen <narciso.guillen@tangosource.com>
2013-02-13 15:50:38 -0600

    build search login on task collection

--- public/javascripts/app/collections/tasks.js
+++ public/javascripts/app/collections/tasks.js
@@ -2,6 +2,18 @@ define(['Backbone'], function(Backbone){
   var Tasks = Backbone.Collection.extend({
     initialize: function(projectId){
       this.url = 'api/v1/projects/'+projectId+'/tasks';
+    },
+
+    search: function(data){
+      var field  = _.keys(data).pop();
+      var filter = new RegExp(data[field]);
+
+      var result = _.filter(this.models, function(model){
+        var value = model.get(field);
+        return filter.test(value) ? model : false;
+      });
+
+      return result;
     }
   });
 
