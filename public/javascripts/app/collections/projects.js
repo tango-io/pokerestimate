6330fb4a175a73f61baae4a81d13cb1817e7f5e0
Narciso Guillen <narciso.guillen@tangosource.com>
2013-02-13 12:19:16 -0600

    Use project model fro projects collection

--- public/javascripts/app/collections/projects.js
+++ public/javascripts/app/collections/projects.js
@@ -1,6 +1,7 @@
-define(['Backbone'], function(Backbone){
+define(['Backbone', 'app/models/project'], function(Backbone, model){
   var Projects = Backbone.Collection.extend({
-    url: 'api/v1/projects'
+    url: 'api/v1/projects',
+    model: model
   });
 
   return Projects;
