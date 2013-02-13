27b1564ded3686b61977adac47527c7ccc98fbb1
Narciso Guillen <narciso.guillen@tangosource.com>
2013-02-13 12:49:35 -0600

    working on project manager

--- public/javascripts/app/views/projectManager.js
+++ public/javascripts/app/views/projectManager.js
@@ -2,27 +2,47 @@ define([
 
   'Backbone',
 
-  //Template
-  'text!templates/project-page/gamePageTemplate.html'
+  //Model
+  'app/models/project',
+
+  //Templates
+  'text!templates/project-page/gamePageTemplate.html',
+  'text!templates/project-page/errorTemplate.html'
 
 ], function(
 
   Backbone,
 
-  //Template
-  gamePageTemplate
+  //Model
+  Project,
+
+  //Templates
+  gamePageTemplate,
+  errorTemplate
 ){
 
   var ProjectManager = Backbone.View.extend({
 
     template: _.template(gamePageTemplate),
+    errorTemplate: _.template(errorTemplate),
+
+    model: new Project(),
 
     initialize: function(){
-      this.render();
+      this.model.clear({silent: true});
+      this.model.bind('change:error', this.error, this);
+      this.model.bind('change:name', this.render, this);
+
+      this.model.fetch({data: {id: this.options.projectId}});
+    },
+
+    error: function(){
+      var message = this.model.get('error');
+      this.$el.html(this.errorTemplate({message: message}));
     },
 
     render: function(){
-      this.$el.html(this.template({project: {name: 'Testing'}}));
+      this.$el.html(this.template({project: {name: this.model.get('name')}}));
     }
   });
 
