444708d3164cef4d99239dd7bb0892d323e34a3f
Narciso Guillen <narciso.guillen@tangosource.com>
2013-02-13 12:18:53 -0600

    Use same api for projects and single project

--- controllers/pivotal_controller.js
+++ controllers/pivotal_controller.js
@@ -4,43 +4,8 @@ var inspect  = require('eyes').inspector({ stream: null });
 
 module.exports = {
   projects: function(req, res, next){
+    var id    = req.query.id;
     var token = req.user ? req.user.token : null;
-
-    if(!token){
-      res.send({error: 'Not logged in'});
-      return false;
-    }
-
-    pivotal.getProjects({token: token}, function(error, data){
-
-      if(error){
-        res.send(error);
-        return false;
-      }
-
-      if(data.message){
-        res.send(data);
-        return false;
-      }
-
-      var projects = data.projects ? data.projects.project : [];
-
-      var result = _.map(projects, function(project){
-        return {
-          name:   typeof project.name   === 'object' ? project.name.pop()   : project.name,
-          id:     typeof project.id     === 'object' ? project.id.pop()     : project.id,
-          public: typeof project.public === 'object' ? project.public.pop() : project.public,
-        };
-      });
-
-      res.send(result);
-
-    });
-  },
-
-  project: function(req, res, next){
-    var token = req.user ? req.user.token : null;
-    var id    = req.params.id;
 
     if(!token){
       res.send({error: 'Not logged in'});
@@ -59,15 +24,21 @@ module.exports = {
         return false;
       }
 
-      var project = _.map(data, function(field){
+      var projects = data.projects ? data.projects.project : data;
+
+      var result = _.map(projects, function(project){
         return {
-          name:   typeof field.name   === 'object' ? field.name.pop()   : field.name,
-          id:     typeof field.id     === 'object' ? field.id.pop()     : field.id,
-          public: typeof field.public === 'object' ? field.public.pop() : field.public
+          name:   typeof project.name   === 'object' ? project.name.pop()   : project.name,
+          id:     typeof project.id     === 'object' ? project.id.pop()     : project.id,
+          public: typeof project.public === 'object' ? project.public.pop() : project.public,
         };
       });
 
-      res.send(project);
+      if(id){
+        result = result.pop();
+      }
+
+      res.send(result);
 
     });
   },
