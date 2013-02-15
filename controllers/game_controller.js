3e7d1627ac808267de81be0b9b3159812781f86e
Narciso Guillen <narciso.guillen@tangosource.com>
2013-02-15 14:14:29 -0600

    Create game logic / fetch games form current project

--- controllers/game_controller.js
+++ controllers/game_controller.js
@@ -3,17 +3,33 @@ var inspect  = require('eyes').inspector({ stream: null });
 module.exports = {
 
   index: function(req, res, next){
+    var project_id = req.query.project_id;
 
-    process.database.games.find(function(error, games){
+    process.database.games.find({project_id: project_id}, function(error, games){
       if(error){res.send(error); return false;}
 
       games.toArray(function(err, results){
         if(err){res.send(err); return false;}
 
-        console.log(inspect(results));
         res.send(results);
       });
 
+    });
+
+  },
+
+  create: function(req, res, next){
+
+    var game = {
+      _id: req.body._id,
+      name: req.body.name,
+      project_id: req.body.projectId
+    };
+
+    process.database.games.save(game, function(error, savedGame){
+      if(error){res.send(error); return false;}
+
+      res.send({sucess: true});
     });
 
   }
