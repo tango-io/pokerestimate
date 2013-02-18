6f6b1afaef862ada88982039766d5c227f76dd16
Narciso Guillen <narciso.guillen@tangosource.com>
2013-02-18 15:56:48 -0600

    Socket lib

---
+++ libs/socket.js
@@ -0,0 +1,15 @@
+var gameController = require('../controllers/game_controller');
+var inspect        = require('eyes').inspector({ stream: null });
+
+exports.start = function(){
+
+  process.io.sockets.on('connection', function(socket){
+    console.log('Someone just connected!');
+
+    socket.on('create game', gameController.create);
+    socket.on('update game', gameController.update);
+
+  });
+
+}
+
