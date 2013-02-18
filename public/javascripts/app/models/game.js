b27fb4a21f98532dffb3b397eeb1e6c31dc4a493
Narciso Guillen <narciso.guillen@tangosource.com>
2013-02-18 15:55:21 -0600

    emit create game from game model

--- public/javascripts/app/models/game.js
+++ public/javascripts/app/models/game.js
@@ -1,7 +1,13 @@
 define(['Backbone'], function(Backbone){
 
   var Game = Backbone.Model.extend({
-    idAttribute: "_id"
+    idAttribute: "_id",
+
+    save: function(error, data){
+      if(error){console.log(error); return false;}
+      var game = data.collection.at(data.collection.length - 1).toJSON();
+      socket.emit('create game', game);
+    }
   });
 
   return Game;
