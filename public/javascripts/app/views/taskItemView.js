3ade08a32c2c533856152249c2e9691213a94cc8
Narciso Guillen <narciso.guillen@tangosource.com>
2013-02-19 18:07:16 -0600

    Remove drag and drop

--- public/javascripts/app/views/taskItemView.js
+++ public/javascripts/app/views/taskItemView.js
@@ -4,20 +4,23 @@ define([
 
   //Template
   'text!templates/project-page/taskItemTemplate.html',
+  'text!templates/project-page/informationTemplate.html',
 
-  'jquery-ui'
+  'bootstrap'
 ], function(
 
   Backbone,
 
   //Template
-  itemTemplate
+  itemTemplate,
+  informationTemplate
 
 ){
 
   var TaskItemView = Backbone.View.extend({
 
     template: _.template(itemTemplate),
+    information: _.template(informationTemplate),
 
     events: {
       'click' : 'selected'
@@ -26,26 +29,11 @@ define([
     initialize: function(title){
       this.$el.html(this.template({title: this.model.get('title')}));
 
-      this.$el.draggable({
-        revert: "invalid",
-        scroll: false,
-        cursor: "move",
-        cursorAt: { top: 25, left: 10 },
-
-        helper: function(event){
-          var $target = $(event.currentTarget);
-          $target.find('li').addClass('selected');
-
-          var num = $('.selected').length - 1 || '';
-          var helper = $target.clone();
-          var span   = helper.find('span');
-          var name   = span.text();
-
-          helper.width('710px');
-
-          if(num){ span.text(name + ' + ' + num); }
-          return helper;
-        }
+      this.$('li').popover({
+        title: this.model.get('title'),
+        trigger: 'hover',
+        content: ' ',
+        template: this.information(this.model.toJSON())
       });
 
       return this;
