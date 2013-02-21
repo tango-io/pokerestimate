25e3e5d54f9389ea8be017c1b97167bc7d76911f
Narciso Guillen <narciso.guillen@tangosource.com>
2013-02-21 14:06:39 -0600

    Set up task detail template

--- public/javascripts/app/views/taskDetail.js
+++ public/javascripts/app/views/taskDetail.js
@@ -1,18 +1,36 @@
-define(['Backbone'], function(Backbone){
+define([
+
+  'Backbone',
+
+  //Template
+  'text!templates/project-page/taskDetailTemplate.html'
+], function(
+
+  Backbone,
+
+  taskDetailTemplate
+){
   var TaskDetail = Backbone.View.extend({
+
+    template: _.template(taskDetailTemplate),
 
     initialize: function(){
       this.collection.bind('change:selected', this.render, this);
     },
 
     render: function(){
-      var selectedTask = this.collection.where({selected: true});
+      var selectedTask = this.collection.where({selected: true}).pop();
 
       //Clean up collection
       _.each(this.collection.models, function(model){
         model.set({selected: false}, {silent: true});
       });
 
+      var task = selectedTask.toJSON();
+      task.labels = task.labels || [];
+      task.description = typeof task.description === 'string' ? task.description : '';
+
+      this.$el.html(this.template(task));
     }
 
   });
