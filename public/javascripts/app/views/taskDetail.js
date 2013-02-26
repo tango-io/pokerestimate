ee8a52a99a615eed29ace4e8911555f9fe797317
Narciso Guillen <narciso.guillen@tangosource.com>
2013-02-25 21:04:21 -0600

    save logic to estimate task

--- public/javascripts/app/views/taskDetail.js
+++ public/javascripts/app/views/taskDetail.js
@@ -36,20 +36,34 @@ define([
 
     save: function(event){
       event.preventDefault();
+      var card = this.$('.selected');
+
+      if(card[0]){
+        var estimated = this.selectedTask.get('estimated') || [];
+        var player    = this.options.router.account.get('user');
+        var num       = card.find('a')[0] ? card.find('a').text() : card.find('input').val();
+
+        estimated.push({
+          player: player,
+          card: num
+        });
+
+        this.selectedTask.save({estimated: estimated});
+      }
     },
 
     render: function(){
-      var selectedTask = this.collection.where({selected: true}).pop();
+      this.selectedTask = this.collection.where({selected: true}).pop();
 
       //Clean up collection
       _.each(this.collection.models, function(model){
         model.set({selected: false}, {silent: true});
       });
 
-      var task = selectedTask.toJSON();
+      var task = this.selectedTask.toJSON();
       task.labels = task.labels ? task.labels[0].split(',') : [];
-
       task.description = typeof task.description === 'string' ? task.description : 'No description';
+      task.estimated = task.estimated || [];
 
       this.$el.html(this.template(task));
     }
