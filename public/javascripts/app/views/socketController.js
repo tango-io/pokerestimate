99bc9631de370b4f5e1b6c271ebbe2ea1f1f230d
Narciso Guillen <narciso.guillen@tangosource.com>
2013-02-27 18:57:52 -0600

    Add socket controller

---
+++ public/javascripts/app/views/socketController.js
@@ -0,0 +1,77 @@
+define(['Backbone', 'socket'], function(Backbone, io){
+  var SocketController = Backbone.View.extend({
+
+    el: '#main-content',
+
+    socketEvents: {
+      'update estimations' : 'updateEstimations',
+      'update players'     : 'updatePlayers',
+      'closing'            : 'closeGame'
+    },
+
+    __initialize: function(){
+      window.socket = io.connect();
+      var view      = this;
+
+      _.each(this.socketEvents, function(method, key){
+        if(typeof view[method] === 'function'){
+          socket.on(key, _.bind(view[method], view));
+        }
+      });
+
+    },
+
+    initialize: function(){
+      this.message = this.options.router.playerMessage;
+
+      this.message.on('change', this.showMessage, this);
+      this.__initialize();
+    },
+
+    showMessage: function(){
+      var message = this.message.get('message');
+      this.$('.js-message').text(message);
+    },
+
+    updatePlayers: function(message, list){
+      //Populate message
+      this.message.set({ message: message });
+
+      //Update player list
+      var pls = _.map(list, function(v, k){return v;});
+      this.options.router.players.update(pls);
+    },
+
+    updateEstimations: function(message, task, estimations){
+      var router = this.options.router;
+      var tasks  = router.project.taskList.collection;
+
+      //Populate message
+      this.message.set({ message: message });
+
+      var taskModel = tasks.get(task);
+
+      //Update task if selected
+      if(taskModel){
+        taskModel.set({estimated: estimations});
+      }
+
+    },
+
+    closeGame: function(message, task, time){
+      var router = this.options.router;
+      var tasks  = router.project.taskList.collection;
+
+      var taskModel = tasks.get(task);
+
+      if(taskModel){
+        var closed = taskModel.get('estimated') ? true : false;
+        taskModel.set({closed: closed, time: time}).trigger('timer');
+      }
+
+    }
+
+  });
+
+  return SocketController;
+});
