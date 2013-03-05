d0326d71a810014b01fc2953665d419c40ccb6d6
Narciso Guillen <narciso.guillen@tangosource.com>
2013-03-05 16:32:08 -0600

    Improve text form the boxes on homepage

--- public/javascripts/app/views/homeView.js
+++ public/javascripts/app/views/homeView.js
@@ -2,26 +2,38 @@ define([
   'Backbone',
 
   //Views
-  'app/views/projectsView'
+  'app/views/projectsView',
+
+  //Templates
+  'text!templates/home/boxesTemplate.html'
 ], function(
   
   Backbone,
 
-  //Views
-  projectsView
+  projectsView,
+
+  //Template
+  boxesTemplate
 ){
 
   var HomeView = Backbone.View.extend({
 
+    template: _.template(boxesTemplate),
+
     initialize: function(){
+      this.render();
+
       this.projects = new projectsView({
         el: '.js-projects',
         account: this.options.account,
         router: this.options.router
       });
 
-    }
+    },
 
+    render: function(){
+      this.$el.html(this.template());
+    }
   });
 
   return HomeView;
