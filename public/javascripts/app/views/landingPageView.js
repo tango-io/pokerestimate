142bb8f9c2892af60095f85a8a4596202344c834
Jaime glez <jaime.gonzalez@tangosource.com>
2013-04-01 13:36:25 -0600

    fixed pull request -- Added 'var' changed prefix and added $ where was neccesary

--- public/javascripts/app/views/landingPageView.js
+++ public/javascripts/app/views/landingPageView.js
@@ -13,13 +13,12 @@ define([
 
     changeStep: function(e){
       e.preventDefault();
-      var $step = e.currentTarget;
-      var step  = $('#coins li').index($step);
-      movePx = -1 * ((step + 1) * 390 - 390);
-      $('#info-steps').css('margin-left', movePx+'px');
-      $('#screen-steps li').fadeOut('fast');
-      screenToShow = $('#screen-steps li')[step];
-      $(screenToShow).fadeIn('fast');
+      var step  = this.$('#coins li').index(e.currentTarget);
+      var movePx = -1 * ((step + 1) * 390 - 390);
+      this.$('#info-steps').css('margin-left', movePx+'px');
+      this.$('#screen-steps li').fadeOut('fast');
+      var screenToShow = this.$('#screen-steps li')[step];
+      this.$(screenToShow).fadeIn('fast');
 
       
 
